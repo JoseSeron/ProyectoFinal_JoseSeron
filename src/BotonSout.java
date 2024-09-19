28cc6c1ed1f10ca0714fcf3f9e81ce7f52db0367
Jose R Seron <joserobertoseron@gmail.com>
2024-09-18 20:39:30 -0600

    clase BotonSout v1.0

--- src/BotonSout.java
+++ src/BotonSout.java
@@ -1,12 +1,43 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
+import java.awt.Color;
+import javax.swing.*;
+
 
 /**
  *
  * @author joser
  */
-public class BotonSout {
-    
+public class BotonSout extends JButton {
+
+    private String variable;
+
+    public BotonSout(String objeto) {
+        this.variable = objeto;
+        //Config formato boton
+        this.setBounds(50, 50, 120, 120);
+        this.setText("<html><br>"
+                + "SOUT"
+                + "<br>"
+                + variable.split("\\)")[1]
+                + "</html>");
+        String rutaIcono = "./Iconos/sout.png";
+        Icon icono = new ImageIcon(rutaIcono);
+        this.setIcon(icono);
+        this.setBackground(Color.ORANGE);
+        this.setHorizontalTextPosition(SwingConstants.CENTER);
+    }
+
+    @Override
+    public String toString() {
+        return "BotonSout{" + "variable=" + variable + '}';
+    }
+
+    public String getVariable() {
+        return variable;
+    }
+
+    public void setVariable(String variable) {
+        this.variable = variable;
+    }
+
 }
